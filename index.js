5e1396aa4c602419012c1ce4dd03edd3536602f7
Naiim <khaskhoussi.naiim@gmail.com>
2022-08-28 17:19:34 +0200

    Adding cors middleware
    Signed-off-by: Naiim <khaskhoussi.naiim@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 const User = require('./controllers/user')
 const Category = require('./controllers/category')
@@ -13,6 +14,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 // app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}))
