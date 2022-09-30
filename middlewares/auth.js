e55ada46680f4997c004f5bb4cd83bae7ff6f883
Naiim <khaskhoussi.naiim@gmail.com>
2022-09-30 02:27:39 +0200

    Fix: fixing isOwner middelware
    Signed-off-by: Naiim <khaskhoussi.naiim@gmail.com>

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -26,14 +26,14 @@ const isAdmin = (req, res, next) => {
     }
 };
 
-const isOwner = (station, req, res, next) => {
+const isOwner = (ticket, req, res, next) => {
     try {
         const token = req.header("Authorization").split(" ")[1];
         if (!token) return res.status(403).send("Access denied.");
         const decoded = jwt.verify(token, process.env.SECRET);
         req.user = decoded;
-        if (req.user.id==station.owner || req.user.role=='admin') next()
-        else res.status(401).send("Only station owner allowed to process this");
+        if (req.user.id==ticket.createdby || req.user.id==ticket.assignee || req.user.role=='admin') next()
+        else res.status(401).send("Only ticket owner, assignee or admin allowed to process this");
     } catch (error) {
         res.status(400).send("Invalid token");
     }
